7c735ab6f016f465aefc3ecd2d92e4a353689fc1
Dagwan Pan'and Danladi <dagwanpan@gmail.com>
2024-06-13 20:04:16 +0100

    testing complete

--- routes/e_cardRoutes.js
+++ routes/e_cardRoutes.js
@@ -42,29 +42,3 @@ router.delete('/:id', e_cardController.deleteEcard);
 
 // Export the Router
 module.exports = router;
-
-// const express = require('express');
-// const router = express.Router();
-
-// // Import the e_cardController to handle requests
-// const e_cardController = require('../controllers/e_cardController');
-
-// // Define the API routes and their corresponding controller methods
-
-// // Create a new e_card
-// router.post('/', e_cardController.createEcard);
-
-// // Get all e_cards
-// router.get('/', e_cardController.getAllEcards);
-
-// // Get a single e_card by ID
-// router.get('/:id', e_cardController.getSingleEcard);
-
-// // Update an e_card by ID
-// router.put('/:id', e_cardController.updateEcard);
-
-// // Delete an e_card by ID
-// router.delete('/:id', e_cardController.deleteEcard);
-
-// // Export the Router
-// module.exports = router;
